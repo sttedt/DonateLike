b213961ac3ecace8c57b173c13c9536e7c904567
sttedt <sttedt@naver.com>
2019-11-16 13:31:05 +0900

    dddd

--- DonateLike/src/main/java/com/donate/like/dao/AdminDao.java
+++ DonateLike/src/main/java/com/donate/like/dao/AdminDao.java
@@ -21,6 +21,13 @@ public class AdminDao {
 		return sessionTemplate.selectList("admin.donateList", no);
 	}
 	
+	public int TargetSelectionUpdate(Map<String, Object> map) {
+		return sessionTemplate.update("admin.TargetSelectionUpdate", map);
+	}
+	//대상선정 삭제
+	public int TargetSelectionDelete(Map<String, Object> map){
+		return sessionTemplate.delete("admin.TargetSelectionDelete", map);
+	}
+	
 }
-
 
